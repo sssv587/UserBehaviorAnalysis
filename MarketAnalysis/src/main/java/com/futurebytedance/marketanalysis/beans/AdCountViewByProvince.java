825337a86ecdf7127513de871911e15760aac127
sssv587 <1092663295@qq.com>
2021-05-04 18:53:19 +0800

    AdCountViewByProvince

---
+++ MarketAnalysis/src/main/java/com/futurebytedance/marketanalysis/beans/AdCountViewByProvince.java
@@ -0,0 +1,55 @@
+package com.futurebytedance.marketanalysis.beans;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2021/5/4 - 18:52
+ * @Description
+ */
+public class AdCountViewByProvince {
+    private String province;
+    private String windowEnd;
+    private Long count;
+
+    public AdCountViewByProvince() {
+    }
+
+    public AdCountViewByProvince(String province, String windowEnd, Long count) {
+        this.province = province;
+        this.windowEnd = windowEnd;
+        this.count = count;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getWindowEnd() {
+        return windowEnd;
+    }
+
+    public void setWindowEnd(String windowEnd) {
+        this.windowEnd = windowEnd;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "AdCountViewByProvince{" +
+                "province='" + province + '\'' +
+                ", windowEnd='" + windowEnd + '\'' +
+                ", count=" + count +
+                '}';
+    }
+}
